51a184b873e2e44a68da205c941b5e7919882d8b
nimbleclouds <50649764+nimbleclouds@users.noreply.github.com>
2024-10-23 14:08:34 +0800

    Update homePage.py

--- homePage.py
+++ homePage.py
@@ -15,25 +15,25 @@ from scipy.stats import zscore
 from sklearn.decomposition import PCA
 from streamlit_metrics import metric, metric_row
 
-def check_password():
-    def password_entered():
-        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]
-        else:
-            st.session_state["password_correct"] = False
-    if st.session_state.get("password_correct", False):
-        return True
-    st.text_input(
-        "Password", type="password", on_change=password_entered, key="password"
-    )
-    if "password_correct" in st.session_state:
-        st.error("Нууц үг буруу байна")
-    return False
+# def check_password():
+#     def password_entered():
+#         if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+#             st.session_state["password_correct"] = True
+#             del st.session_state["password"]
+#         else:
+#             st.session_state["password_correct"] = False
+#     if st.session_state.get("password_correct", False):
+#         return True
+#     st.text_input(
+#         "Password", type="password", on_change=password_entered, key="password"
+#     )
+#     if "password_correct" in st.session_state:
+#         st.error("Нууц үг буруу байна")
+#     return False
 
 
-if not check_password():
-    st.stop()
+# if not check_password():
+#     st.stop()
 
 
 st.set_page_config(
